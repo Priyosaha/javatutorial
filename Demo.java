fce7efb137891d868f22436f869fe6f286cd5037
Priyosaha <priyosaha339@gmail.com>
2024-02-26 21:46:21 +0530

    code to add two numbers

---
+++ Demo.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class Demo{
+
+public static void main(String[] args){
+
+Scanner sc = new Scanner(System.in);
+int a  = sc.nextInt();
+int b = sc.nextInt();
+System.out.println("result= "+(a+b));
+
+}
+}
