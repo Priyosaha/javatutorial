69f82a61e25c372a8b845e2ba891d4442fd98ef1
Priyosaha <priyosaha339@gmail.com>
2024-06-25 00:26:15 +0530

    adding numbers

--- Demo.java
+++ Demo.java
@@ -1,12 +1,21 @@
 import java.util.Scanner;
+
+import java.io.StringWriter;
+import java.io.PrintWriter;
 public class Demo{
 
 public static void main(String[] args){
 
-Scanner sc = new Scanner(System.in);
-int a  = sc.nextInt();
-int b = sc.nextInt();
-System.out.println("result= "+(a+b));
+String x = "abcdef";
+String y = "abcdes";
+
+if(x==y){
+    System.out.println("true");
+}else{
+    System.out.println("false");
+}
+
+System.out.println(x.substring(0, 6));
 
 }
 }
