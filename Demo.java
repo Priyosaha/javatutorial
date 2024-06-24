4b75aa5636a14678b43df72c79aba6e01d77e6eb
Priyosaha <priyosaha339@gmail.com>
2024-06-25 00:34:09 +0530

    adding .equals() method

--- Demo.java
+++ Demo.java
@@ -1,21 +1,18 @@
-import java.util.Scanner;
 
-import java.io.StringWriter;
-import java.io.PrintWriter;
-public class Demo{
+public class Demo {
 
-public static void main(String[] args){
+    public static void main(String[] args) {
 
-String x = "abcdef";
-String y = "abcdes";
+        String x = "abcdef";
+        String y = "abcdes";
 
-if(x==y){
-    System.out.println("true");
-}else{
-    System.out.println("false");
-}
+        if (x != null && x.equals(y)) {
+            System.out.println("true");
+        } else {
+            System.out.println("false");
+        }
 
-System.out.println(x.substring(0, 6));
+        System.out.println(x.substring(0, 6));
 
-}
+    }
 }
